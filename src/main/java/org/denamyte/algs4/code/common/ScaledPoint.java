7bdc3c370e7afb981ca19c9271cd8f56dec9ea9c
denamyte <denamyte@gmail.com>
2020-11-20 17:22:55 +0200

    Task 1.1.32 removed

--- src/main/java/org/denamyte/algs4/code/common/ScaledPoint.java
+++
@@ -1,7 +0,0 @@
-package org.denamyte.algs4.code.common;
-
-public class ScaledPoint extends Point {
-    public ScaledPoint(double x, double y, Point factors) {
-        super(factors.x * x, factors.y * y);
-    }
-}
--- src/main/java/org/denamyte/algs4/code/common/ScaledRect.java
+++
@@ -1,7 +0,0 @@
-package org.denamyte.algs4.code.common;
-
-public class ScaledRect extends Rect {
-    public ScaledRect(double x, double y, double w, double h, Point factors) {
-        super(factors.x * x, factors.y * y, factors.x * w, factors.y * h);
-    }
-}
