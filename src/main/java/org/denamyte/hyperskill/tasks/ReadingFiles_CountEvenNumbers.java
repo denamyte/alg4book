b05b4a2192ad3d4695deb0be52ed216cde99dcab
denamyte <denamyte@gmail.com>
2020-11-24 20:43:41 +0200

    Hyperskill task of the day 2020-11-24: Reading files -> Count even numbers

---
+++ src/main/java/org/denamyte/hyperskill/tasks/ReadingFiles_CountEvenNumbers.java
@@ -0,0 +1,20 @@
+package org.denamyte.hyperskill.tasks;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.stream.Stream;
+
+// src/main/java/org/denamyte/hyperskill/files/dataset_91065.txt
+public class ReadingFiles_CountEvenNumbers {
+    public static void main(String[] args) {
+        String filePath = args[0];
+        try (Scanner scanner = new Scanner(new File(filePath))) {
+            long count = Stream.generate(scanner::nextInt).takeWhile(value -> value != 0)
+                    .filter(value -> value % 2 == 0).count();
+            System.out.println(count);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
