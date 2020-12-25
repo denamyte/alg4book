c9612a187f8428bb35d8dc1b3f09f5943917647d
denamyte <denamyte@gmail.com>
2020-12-25 21:27:06 +0200

    EditDistance visualization added

--- src/main/java/org/denamyte/hyperskill/algorithms/EditDistance.java
+++ src/main/java/org/denamyte/hyperskill/algorithms/EditDistance.java
@@ -8,6 +8,46 @@ public class EditDistance {
     }
 
     public static int editDistance(String s, String t) {
+        int[][] distTable = calcDistanceTable(s, t);
+        return distTable[s.length()][t.length()];
+    }
+
+    public static int editDistanceOutput(String s, String t) {
+        int[][] distTable = calcDistanceTable(s, t);
+        s = s.toUpperCase();
+        t = t.toUpperCase();
+
+        String[][] showTable = new String[s.length() + 2][t.length() + 2];
+        // the 2 leading spaces in the first column and first row
+        for (int i = 0; i < 2; i++)
+            showTable[0][i] = showTable[i][0] = " ";
+
+        // the first word (vertical)
+        for (int i = 2; i < s.length() + 2; i++)
+            showTable[i][0] = String.valueOf(s.charAt(i - 2));
+
+        // the second word (horizontal)
+        for (int j = 2; j < t.length() + 2; j++)
+            showTable[0][j] = String.valueOf(t.charAt(j - 2));
+
+        // copy distTable into showTable
+        for (int i = 0; i < s.length() + 1; i++) {
+            for (int j = 0; j < t.length() + 1; j++) {
+                showTable[i + 1][j + 1] = String.valueOf(distTable[i][j]);
+            }
+        }
+
+        for (String[] line : showTable) {
+            for (String value : line) {
+                System.out.printf("%s  ", value);
+            }
+            System.out.println();
+        }
+
+        return distTable[s.length()][t.length()];
+    }
+
+    private static int[][] calcDistanceTable(String s, String t) {
         int vert = s.length() + 1;
         int horz = t.length() + 1;
         int[][] d = new int[vert][horz];
@@ -28,21 +68,12 @@ public class EditDistance {
                 d[i][j] = Math.min(Math.min(insCost, delCost), subCost);
             }
         }
-
-        // Debug output of the distance table
-        for (int[] line : d) {
-            for (int value : line) {
-                System.out.printf("%d  ", value);
-            }
-            System.out.println();
-        }
-
-        return d[s.length()][t.length()];
+        return d;
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println(editDistance(
+        System.out.println(editDistanceOutput(
                 scanner.next(), scanner.next()
         ));
     }
