701d424e1898dbbe741e047715c607843a90114c
denamyte <denamyte@gmail.com>
2021-01-06 17:57:13 +0200

    Hyperskill task: Edit distance alignment in Java -> Multiple sequence alignment: SOLVED

--- src/main/java/org/denamyte/hyperskill/tasks/EditDistanceAlignmentInJava_MultipleSequenceAlignment.java
+++ src/main/java/org/denamyte/hyperskill/tasks/EditDistanceAlignmentInJava_MultipleSequenceAlignment.java
@@ -1,10 +1,9 @@
 package org.denamyte.hyperskill.tasks;
 
-import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
+import java.util.stream.IntStream;
 
 /**
  * <a href="https://hyperskill.org/learn/step/5704">Edit distance alignment in Java -> Multiple sequence alignment</a>
@@ -19,53 +18,45 @@ public class EditDistanceAlignmentInJava_MultipleSequenceAlignment {
 
     public static Alignment3 editDistanceAlignment(String s, String t, String u) {
 
-        CellData[][][] cubeSD = new CellData[s.length() + 1][t.length() + 1][u.length() + 1];
+        int[][][] cube = new int[s.length() + 1][t.length() + 1][u.length() + 1];
 
-        cubeSD[0][0][0] = new CellData(0, 0);
+        cube[0][0][0] = 0;
+        AlgorithmData data = new AlgorithmData(cube, s, t, u);
 
-        // Initializing cube edges
+        // Initializing the cube edges
         for (int i = 1; i < s.length() + 1; i++) {
-            int cost = i * DOUBLE_GAP_COST;
-            cubeSD[i][0][0] = new CellData(cost, cost - cubeSD[i - 1][0][0].cost);
+            cube[i][0][0] = i * DOUBLE_GAP_COST;
         }
         for (int j = 1; j < t.length() + 1; j++) {
-            int cost = j * DOUBLE_GAP_COST;
-            cubeSD[0][j][0] = new CellData(cost, cost - cubeSD[0][j - 1][0].cost);
+            cube[0][j][0] = j * DOUBLE_GAP_COST;
         }
         for (int k = 1; k < u.length() + 1; k++) {
-            int cost = k * DOUBLE_GAP_COST;
-            cubeSD[0][0][k] = new CellData(cost, cost - cubeSD[0][0][k - 1].cost);
+            cube[0][0][k] = k * DOUBLE_GAP_COST;
         }
 
-        // Initialization cube surfaces
+        // Initializing the cube surfaces
         for (int i = 1; i < s.length() + 1; i++) {
             for (int j = 1; j < t.length() + 1; j++) {
-                int replaceCost = pairMatch(s.charAt(i - 1), t.charAt(j - 1)) + DOUBLE_GAP_COST;
-                CellData insCostCD = new CellData(cubeSD[i][j - 1][0].cost + DOUBLE_GAP_COST, DOUBLE_GAP_COST);
-                CellData delCostCD = new CellData(cubeSD[i - 1][j][0].cost + DOUBLE_GAP_COST, DOUBLE_GAP_COST);
-                CellData subCostCD = new CellData(cubeSD[i - 1][j - 1][0].cost + replaceCost, replaceCost);
-                CellData minData = Stream.of(insCostCD, delCostCD, subCostCD).min(cellDataComparator).orElseThrow();
-                cubeSD[i][j][0] = minData;
+                int insCost = cube[i][j - 1][0] + DOUBLE_GAP_COST;
+                int delCost = cube[i - 1][j][0] + DOUBLE_GAP_COST;
+                int subCost = cube[i - 1][j - 1][0] + pairMatch(s.charAt(i - 1), t.charAt(j - 1)) + DOUBLE_GAP_COST;
+                cube[i][j][0] = IntStream.of(insCost, delCost, subCost).min().orElseThrow();
             }
         }
         for (int j = 1; j < t.length() + 1; j++) {
             for (int k = 1; k < u.length() + 1; k++) {
-                int replaceCost = pairMatch(t.charAt(j - 1), u.charAt(k - 1)) + DOUBLE_GAP_COST;
-                CellData insCostCD = new CellData(cubeSD[0][j][k - 1].cost + DOUBLE_GAP_COST, DOUBLE_GAP_COST);
-                CellData delCostCD = new CellData(cubeSD[0][j - 1][k].cost + DOUBLE_GAP_COST, DOUBLE_GAP_COST);
-                CellData subCostCD = new CellData(cubeSD[0][j - 1][k - 1].cost + replaceCost, replaceCost);
-                CellData minData = Stream.of(insCostCD, delCostCD, subCostCD).min(cellDataComparator).orElseThrow();
-                cubeSD[0][j][k] = minData;
+                int insCost = cube[0][j][k - 1] + DOUBLE_GAP_COST;
+                int delCost = cube[0][j - 1][k] + DOUBLE_GAP_COST;
+                int subCost = cube[0][j - 1][k - 1] + pairMatch(t.charAt(j - 1), u.charAt(k - 1)) + DOUBLE_GAP_COST;
+                cube[0][j][k] = IntStream.of(insCost, delCost, subCost).min().orElseThrow();
             }
         }
         for (int i = 1; i < s.length() + 1; i++) {
             for (int k = 1; k < u.length() + 1; k++) {
-                int replaceCost = pairMatch(s.charAt(i - 1), u.charAt(k - 1)) + DOUBLE_GAP_COST;
-                CellData insCostCD = new CellData(cubeSD[i][0][k - 1].cost + DOUBLE_GAP_COST, DOUBLE_GAP_COST);
-                CellData delCostCD = new CellData(cubeSD[i - 1][0][k].cost + DOUBLE_GAP_COST, DOUBLE_GAP_COST);
-                CellData subCostCD = new CellData(cubeSD[i - 1][0][k - 1].cost + replaceCost, replaceCost);
-                CellData minData = Stream.of(insCostCD, delCostCD, subCostCD).min(cellDataComparator).orElseThrow();
-                cubeSD[i][0][k] = minData;
+                int insCost = cube[i][0][k - 1] + DOUBLE_GAP_COST;
+                int delCost = cube[i - 1][0][k] + DOUBLE_GAP_COST;
+                int subCost = cube[i - 1][0][k - 1] + pairMatch(s.charAt(i - 1), u.charAt(k - 1)) + DOUBLE_GAP_COST;
+                cube[i][0][k] = IntStream.of(insCost, delCost, subCost).min().orElseThrow();
             }
         }
 
@@ -73,31 +64,14 @@ public class EditDistanceAlignmentInJava_MultipleSequenceAlignment {
         for (int i = 1; i <= s.length(); i++) {
             for (int j = 1; j <= t.length(); j++) {
                 for (int k = 1; k <= u.length(); k++) {
-
-                    char s1Char = s.charAt(i - 1);
-                    char s2Char = t.charAt(j - 1);
-                    char s3Char = u.charAt(k - 1);
-                    int costPairIJ = pairMatch(s1Char, s2Char);
-                    int costPairIK = pairMatch(s1Char, s3Char);
-                    int costPairJK = pairMatch(s2Char, s3Char);
-
-                    CellData minData = Stream.of(
-                            new CellData(cubeSD[i - 1][j - 1][k - 1].cost + costPairIJ + costPairIK + costPairJK, costPairIJ + costPairIK + costPairJK),
-                            new CellData(cubeSD[i - 1][j - 1][k    ].cost + costPairIJ + DOUBLE_GAP_COST, costPairIJ + DOUBLE_GAP_COST),
-                            new CellData(cubeSD[i - 1][j    ][k - 1].cost + costPairIK + DOUBLE_GAP_COST, costPairIK + DOUBLE_GAP_COST),
-                            new CellData(cubeSD[i    ][j - 1][k - 1].cost + costPairJK + DOUBLE_GAP_COST, costPairJK + DOUBLE_GAP_COST),
-                            new CellData(cubeSD[i - 1][j    ][k    ].cost + DOUBLE_GAP_COST, DOUBLE_GAP_COST),
-                            new CellData(cubeSD[i    ][j - 1][k    ].cost + DOUBLE_GAP_COST, DOUBLE_GAP_COST),
-                            new CellData(cubeSD[i    ][j    ][k - 1].cost + DOUBLE_GAP_COST, DOUBLE_GAP_COST)
-                    ).min(cellDataComparator).orElseThrow();
-                    cubeSD[i][j][k] = minData;
+                    cube[i][j][k] = new CellsTransitionCost(data, i, j, k).minTransCost();
                 }
             }
         }
-        return reconstructAlignment(cubeSD, s, t, u);
+        return reconstructAlignment(data, s, t, u);
     }
 
-    public static Alignment3 reconstructAlignment(CellData[][][] cubeSD, String s, String t, String u) {
+    public static Alignment3 reconstructAlignment(AlgorithmData cellsData, String s, String t, String u) {
         StringBuilder ssBuilder = new StringBuilder();
         StringBuilder ttBuilder = new StringBuilder();
         StringBuilder uuBuilder = new StringBuilder();
@@ -105,54 +79,47 @@ public class EditDistanceAlignmentInJava_MultipleSequenceAlignment {
         int j = t.length();
         int k = u.length();
 
-        int editDistance = cubeSD[i][j][k].cost;
+        int editDistance = cellsData.cube[i][j][k];
 
         while (i > 0 || j > 0 || k > 0) {
-            CellData data = cubeSD[i][j][k];
-            int prevCost = data.cost - data.transCost;
-            if (i > 0 && j > 0 && k > 0 &&
-                    prevCost == cubeSD[i - 1][j - 1][k - 1].cost) {
+            CellsTransitionCost transCost = new CellsTransitionCost(cellsData, i, j, k);
+            int curCost = cellsData.cube[i][j][k];
+            if (i > 0 && j > 0 && k > 0 && curCost == transCost.transCostIJK()) {
                 ssBuilder.append(s.charAt(i - 1));
                 ttBuilder.append(t.charAt(j - 1));
                 uuBuilder.append(u.charAt(k - 1));
                 i--;
                 j--;
                 k--;
-            } else if (i > 0 && j > 0 &&
-                    prevCost == cubeSD[i - 1][j - 1][k].cost) {
+            } else if (i > 0 && j > 0 && curCost == transCost.transCostIJX()) {
                 ssBuilder.append(s.charAt(i - 1));
                 ttBuilder.append(t.charAt(j - 1));
                 uuBuilder.append('-');
                 i--;
                 j--;
-            } else if (i > 0 && k > 0 &&
-                    prevCost == cubeSD[i - 1][j][k - 1].cost) {
+            } else if (i > 0 && k > 0 && curCost == transCost.transCostIXK()) {
                 ssBuilder.append(s.charAt(i - 1));
                 ttBuilder.append('-');
                 uuBuilder.append(u.charAt(k - 1));
                 i--;
                 k--;
-            } else if (j > 0 && k > 0 &&
-                    prevCost == cubeSD[i][j - 1][k - 1].cost) {
+            } else if (j > 0 && k > 0 && curCost == transCost.transCostXJK()) {
                 ssBuilder.append('-');
                 ttBuilder.append(t.charAt(j - 1));
                 uuBuilder.append(u.charAt(k - 1));
                 j--;
                 k--;
-            } else if (i > 0 &&
-                    prevCost == cubeSD[i - 1][j][k].cost) {
+            } else if (i > 0 && curCost == transCost.transCostIXX()) {
                 ssBuilder.append(s.charAt(i - 1));
                 ttBuilder.append('-');
                 uuBuilder.append('-');
                 i--;
-            } else if (j > 0 &&
-                    prevCost == cubeSD[i][j - 1][k].cost) {
+            } else if (j > 0 && curCost == transCost.transCostXJX()) {
                 ssBuilder.append('-');
                 ttBuilder.append(t.charAt(j - 1));
                 uuBuilder.append('-');
                 j--;
-            } else if (k > 0 &&
-                    prevCost == cubeSD[i][j][k - 1].cost) {
+            } else if (k > 0 && curCost == transCost.transCostXXK()) {  // Should be just 'else' here
                 ssBuilder.append('-');
                 ttBuilder.append('-');
                 uuBuilder.append(u.charAt(k - 1));
@@ -167,45 +134,104 @@ public class EditDistanceAlignmentInJava_MultipleSequenceAlignment {
         return new Alignment3(editDistance, ss, tt, uu);
     }
 
+    static class AlgorithmData {
+        final int[][][] cube;
+        final String s;
+        final String t;
+        final String u;
+
+        AlgorithmData(int[][][] cube, String s, String t, String u) {
+            this.cube = cube;
+            this.s = s;
+            this.t = t;
+            this.u = u;
+        }
+    }
+
+    static class CellsTransitionCost {
+        final AlgorithmData data;
+        final int i;
+        final int j;
+        final int k;
+
+        public CellsTransitionCost(AlgorithmData data, int i, int j, int k) {
+            this.data = data;
+            this.i = i;
+            this.j = j;
+            this.k = k;
+        }
+
+        int ijPairCost() {
+            return pairMatch(data.s.charAt(i - 1), data.t.charAt(j - 1));
+        }
+
+        int ikPairCost() {
+            return pairMatch(data.s.charAt(i - 1), data.u.charAt(k - 1));
+        }
+
+        int jkPairCost() {
+            return pairMatch(data.t.charAt(j - 1), data.u.charAt(k - 1));
+        }
+
+        int transCostIJK() {
+            return data.cube[i - 1][j - 1][k - 1] + ijPairCost() + ikPairCost() + jkPairCost();
+        }
+
+        int transCostIJX() {
+            return data.cube[i - 1][j - 1][k] + ijPairCost() + DOUBLE_GAP_COST;
+        }
+
+        int transCostIXK() {
+            return data.cube[i - 1][j][k - 1] + ikPairCost() + DOUBLE_GAP_COST;
+        }
+
+        int transCostXJK() {
+            return data.cube[i][j - 1][k - 1] + jkPairCost() + DOUBLE_GAP_COST;
+        }
+
+        int transCostIXX() {
+            return data.cube[i - 1][j][k] + DOUBLE_GAP_COST;
+        }
+
+        int transCostXJX() {
+            return data.cube[i][j - 1][k] + DOUBLE_GAP_COST;
+        }
+
+        int transCostXXK() {
+            return data.cube[i][j][k - 1] + DOUBLE_GAP_COST;
+        }
+
+        int minTransCost() {
+            return IntStream.of(
+                    transCostIJK(),
+                    transCostIJX(),
+                    transCostIXK(),
+                    transCostXJK(),
+                    transCostIXX(),
+                    transCostXJX(),
+                    transCostXXK()
+            ).min().orElseThrow();
+        }
+    }
+
     static class Alignment3 {
         public int editDistance;
-        public String s1;
-        public String s2;
-        public String s3;
+        public String s;
+        public String t;
+        public String u;
 
-        public Alignment3(int editDistance, String s1, String s2, String s3) {
+        public Alignment3(int editDistance, String s, String t, String u) {
             this.editDistance = editDistance;
-            this.s1 = s1;
-            this.s2 = s2;
-            this.s3 = s3;
+            this.s = s;
+            this.t = t;
+            this.u = u;
         }
 
         @Override
         public String toString() {
-            return String.format("%d%n%s%n%s%n%s", editDistance, s1, s2, s3);
+            return String.format("%d%n%s%n%s%n%s", editDistance, s, t, u);
         }
     }
-
-    static class CellData {
-        int cost;       // The total value;
-        int transCost;  // The value of transition;
-
-        public int getCost() {
-            return cost;
-        }
-
-        public CellData(int cost, int transCost) {
-            this.cost = cost;
-            this.transCost = transCost;
-        }
-
-        @Override
-        public String toString() {
-            return "cost=" + cost + ", transCost=" + transCost;
-        }
-    }
-
-    static Comparator<CellData> cellDataComparator = Comparator.comparing(CellData::getCost);
 
     public static void main(String[] args) {
         List<String> list = new Scanner(System.in).tokens().limit(3).collect(Collectors.toList());
@@ -213,3 +239,19 @@ public class EditDistanceAlignmentInJava_MultipleSequenceAlignment {
         System.out.println(alignment);
     }
 }
+
+/*
+
+Test #1
+
+ACGGGT
+ACACCGGT
+ACCGG
+
+Test #2
+
+TACTTGGCGA
+CACATTTGGAATCTG
+GTCTCAACGACTAATTAAGT
+
+ */
